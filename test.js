b168f92bcca5e3a5c3ebcc9a61b2732d4146250e
Randy Goldsmith <randyg@Randys-MacBook-Pro.local>
2023-06-01 23:52:58 -0500

    finished excercise

--- test.js
+++ test.js
@@ -1,1 +1,13 @@
-// Your code here
+const Employee = require('./employee');
+
+const johnWick = new Employee('John Wick', 'Dog Lover');
+
+let speak = johnWick.sayName;
+let myfunc = speak.bind(johnWick);
+
+setTimeout(myfunc, 2000);
+
+let job = johnWick.sayOccupation;
+let myfunc2 = job.bind(johnWick);
+
+setTimeout(myfunc2, 3000);
